d8adbcf76b7821ce96cee3254e1b4eb0032c2f7b
kpardeep121974-cmyk <kpardeep121974@gmail.com>
2026-05-13 20:07:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AIEditor from './magicAIeditor'; // AI file ko connect kiya
+import AIEditor from './AIEditor'; // AI file ko connect kiya
 
 function App() {
   return (
