a76ca4e9b7c2ef14535b68190e6f132c6b85f122
kpardeep121974-cmyk <kpardeep121974@gmail.com>
2026-05-13 20:43:40 +0530

    Create MusicData.js

---
+++ src/MusicData.js
@@ -0,0 +1,12 @@
+export const artists = [
+  { id: '1', name: 'Sidhu Moose Wala', genre: 'Punjabi' },
+  { id: '2', name: 'Diljit Dosanjh', genre: 'Punjabi' },
+  { id: '3', name: 'Arijit Singh', genre: 'Hindi' },
+  { id: '4', name: 'BTS', genre: 'K-Pop' },
+  { id: '5', name: 'Karan Aujla', genre: 'Punjabi' },
+  { id: '6', name: 'Shubh', genre: 'Punjabi' },
+  { id: '7', name: 'The Weeknd', genre: 'Pop' },
+  { id: '8', name: 'Interworld', genre: 'Phonk' },
+  { id: '9', name: 'Jubin Nautiyal', genre: 'Sad/Romantic' },
+  // Aap aise hi 100+ naam add kar sakte hain
+];
